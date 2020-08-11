111ae50e88dca558e0d1be3e3744e279169f3492
Khaya Mpehle <khaya.mpehle@gmail.com>
2020-08-11 15:59:15 +0930

    fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name = "GaussRF",
     version = "0.0.1",
-    author = " K. Mpehle"
+    author = " K. Mpehle",
     author_email = "khaya.mpehle@gmail.com",
     description = "Simulation of 1-D and 2-D rectangular random fields",
     long_description = long_description,
